9374cd75680b87a0ca79d555647b98e618945e09
Seoin Jeon <sower031@gmail.com>
2026-05-13 10:32:57 +0900

    기본

--- supabase/client.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kjwnlrbcgtlppxibejty.supabase.co',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtqd25scmJjZ3RscHB4aWJlanR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNjUzNjgsImV4cCI6MjA5Mzc0MTM2OH0.6WsmLd2ubsVCyegvXY9CG--ndmEo8ewY57MdJ7hzb94'
-  )
-}
